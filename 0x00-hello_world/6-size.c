8ccd84b2cbf7a78df21e3f8af60a1b8db1c16bc2
Wolfgang-25 <wmofunanya@gmail.com>
2023-03-17 07:50:53 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,8 +2,8 @@
 
 /**
  * main - Entry point
- * printf - Prints simple informations
- *
+ * printf - Prints the size of various types 
+ * on the computer it is compiled and run on
  * Return: Always 0 (Success)
  */
 int main(void)
