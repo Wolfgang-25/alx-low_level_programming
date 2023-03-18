44a57ffcc0500935b799e23f8f73754533529b0f
Wolfgang-25 <wmofunanya@gmail.com>
2023-03-17 20:03:55 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * putchar:Prints the alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+		putchar('\n');
+	}
+	return (0);
+}
